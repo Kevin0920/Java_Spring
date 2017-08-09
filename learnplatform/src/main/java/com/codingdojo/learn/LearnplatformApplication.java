23c20c816c19fb0b999861c18556b3cef5d0bc05
Lingsi Ke <lingsike@Lingsis-MacBook-Pro.local>
2017-08-09 11:44:29 -0700

    learningplatform

---
+++ learnplatform/src/main/java/com/codingdojo/learn/LearnplatformApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnplatformApplication.class, args);
+	}
+}
---
+++ learnplatform/src/main/java/com/codingdojo/learn/controllers/LearningController.java
@@ -0,0 +1,25 @@
+package com.codingdojo.learn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class LearningController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/m/{part1}/0553/{part2}")
+	public String lesson() {
+		return "lesson.jsp";
+	}
+	
+	@RequestMapping("/m/{part1}/0483/{part2}")
+	public String assignment() {
+		return "assignment.jsp";
+	}
+	
+
+}
